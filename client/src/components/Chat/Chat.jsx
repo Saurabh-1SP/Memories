5b3899c00a60372b46dc35ff2de1427b9a6c0f63
Saurabh-1SP <113672743+Saurabh-1SP@users.noreply.github.com>
2023-04-26 00:09:21 +0530

    some change

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,42 +1,9 @@
-import React, { useState } from 'react'
-
-import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
+import React from 'react'
 
 const Chat = () => {
-	const [username, setUsername] = useState('')
-
-	function createDirectChat(creds) {
-		getOrCreateChat(
-			creds,
-			{ is_direct_chat: true, usernames: [username] },
-			() => setUsername('')
-		)
-	}
-
-	// function renderChatForm(creds) {
-	// 	return (
-	// 		<div>
-	// 			<input 
-	// 				placeholder='Username' 
-	// 				value={username} 
-	// 				onChange={(e) => setUsername(e.target.value)} 
-	// 			/>
-	// 			<button onClick={() => createDirectChat(creds)}>
-	// 				Create
-	// 			</button>
-	// 		</div>
-	// 	)
-	// }
-
-	return (
-		<ChatEngine
-			height='100vh'
-			userName='Saurabh'
-			userSecret='pasi'
-			projectID='92f84a2a-478b-4fe1-96e0-77724c7d7c02'
-			// renderNewChatForm={(creds) => renderChatForm(creds)}
-		/>
-	)
+  return (
+	<div>Chat</div>
+  )
 }
 
-export default Chat;
+export default Chat
